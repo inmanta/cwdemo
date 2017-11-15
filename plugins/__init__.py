d86fe322c5a53aeed93e461482e4f210c64a2e24
Bart Vanbrabant <bart.vanbrabant@inmanta.com>
2017-11-15 11:46:13 +0100

    Minor stability updates

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -8,17 +8,21 @@
 from inmanta.plugins import plugin, Context
 import requests
 
+
 @plugin
 def instances(url: "string", vnf_name: "string") -> "number":
-    r = requests.get(url)
-    data = r.json()
-    if "series" in data and len(data["series"]) > 0:
-        for serie in data["series"]:
-            if "vnf" in serie["tags"] and vnf_name == serie["tags"]["vnf"]:
-                keys = serie["columns"]
-                values = serie["values"][-1]
+    try:
+        r = requests.get(url)
+        data = r.json()
+        if data is not None and "series" in data and data["series"] is not None and len(data["series"]) > 0:
+            for serie in data["series"]:
+                if "vnf" in serie["tags"] and vnf_name == serie["tags"]["vnf"]:
+                    keys = serie["columns"]
+                    values = serie["values"][-1]
 
-                d = dict(zip(keys, values))
-                
-                return d["scale"]
+                    d = dict(zip(keys, values))
+
+                    return d["scale"]
+    except (requests.exceptions.ConnectionError):
+        pass
     return 0
